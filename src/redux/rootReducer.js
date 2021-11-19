0293c84aa5eb18148a6f95bd5c22ea2f66c9b979
moroz-art-dev <moroz.art.dev@gmail.com>
2021-11-19 14:11:09 +0200

    lesson block_11_5

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,5 +2,15 @@ const initialState = {
     counter: 0
 }
 export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD':
+            return {
+                counter: state.counter + 1
+            }
+        case 'SUB':
+            return {
+                counter: state.counter - 1
+            }
+    }
     return state
 }
